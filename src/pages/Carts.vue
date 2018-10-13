8b365c129073f9a358d7c314cb00c0bbf2d81081
ultrays <ultrays@163.com>
2018-10-13 17:04:50 +0800

    万表webapp 创建项目目录

---
+++ src/pages/Carts.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+  export default {
+    name: "Carts"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/pages/Sort.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+  export default {
+    name: "Sort"
+  }
+</script>
+
+<style scoped>
+
+</style>
