44f54d78361812a812adcd501fa5ebea19ccdc28
ultrays <ultrays@163.com>
2018-10-23 10:34:17 +0800

    远帅删库

--- src/commion/commions.js
+++
@@ -1,5 +0,0 @@
-export const CARTS = "http://localhost:3000/api/carts";
-export const PROductDetail = "http://localhost:3000/api/ProductDetail?id=";
-export const SORT = "http://localhost:3000/api/sort";
-export const SORTLIST = "http://localhost:3000/api/sortList?id=";
-
